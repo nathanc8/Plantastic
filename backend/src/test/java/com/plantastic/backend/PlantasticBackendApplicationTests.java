cca8fc8e0970534b9d7257ad88b569acb90127d3
nathanc8 <cazard.nathan@gmail.com>
2025-06-06 17:25:47 +0200

    initialisation du projet

---
+++ backend/src/test/java/com/plantastic/backend/PlantasticBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantastic.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantasticBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
